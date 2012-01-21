954eca6871f77f25b0db9414f023c0eefcceaa8d
Justin J. Moses <justinjmoses@gmail.com>
2012-01-21 17:06:38 -0500

    Adding prod db via mongolab

--- models/db_connect.js
+++ models/db_connect.js
@@ -1,6 +1,10 @@
 var fs = require('fs');
 exports = mongoose = require('mongoose');
 var environment = process.env.NODE_ENV || 'development'
-var mongo_uri = JSON.parse( fs.readFileSync(process.cwd()+'/config.json', encoding='utf8') )[environment].mongo_uri;
+if (environment == 'production') {
+  var mongo_uri = process.env.MONGOLAB_URI;
+} else {
+  var mongo_uri = JSON.parse( fs.readFileSync(process.cwd()+'/config.json', encoding='utf8') )[environment].mongo_uri;  
+}
 mongoose.connect(mongo_uri);
 exports = Schema = mongoose.Schema;
