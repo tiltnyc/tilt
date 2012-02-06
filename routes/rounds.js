5c9949739d4033d61e4594676715031a68709a1e
justin j. moses <justinjmoses@gmail.com>
2012-02-06 18:26:42 -0500

    Supporting basic processing and fixing next-round issues

--- routes/rounds.js
+++ routes/rounds.js
@@ -3,9 +3,10 @@ var Round = require('../models/round'),
     Transaction = require('../models/transaction');
 
 module.exports = function(app){
+  var redirect = '/rounds';
 
   // Rounds control
-  app.get('/rounds', function(req, res){
+  app.get(redirect, function(req, res){
     Round
       .find({})
       .asc('number') 
@@ -19,7 +20,7 @@ module.exports = function(app){
   });
 
   //Append new round
-  app.post('/rounds', function(req, res) {
+  app.post(redirect, function(req, res) {
     Round.count({}, function(err, numRounds) {
       if (err) return err;
 
@@ -30,7 +31,7 @@ module.exports = function(app){
       round.save(function(err, round) {
         if (err) return err;
         req.flash('notice', 'Round appended.');
-        res.redirect('/rounds');
+        res.redirect(redirect);
       });
     });
   });
@@ -52,7 +53,7 @@ module.exports = function(app){
     round = req.round;
     round.remove(function(err){
       req.flash('notice', 'Removed round');
-      res.redirect('/rounds');
+      res.redirect(redirect);
     });
   });
 
@@ -75,36 +76,54 @@ module.exports = function(app){
       });   
   });
 
-  //Toggle round open/close
+  //Toggle round open/close, or current round
   app.put('/round/:roundNumber', function(req, res){
     var round = req.round;
-    if (req.body.round.is_open !== null) round.is_open = (req.body.round.is_open.toLowerCase() === 'true');
+    if (req.body.round.is_open) round.is_open = (req.body.round.is_open.toLowerCase() === 'true');
     if (req.body.round.next_round) round.is_current = false;
-    
+      
     round.save(function(err){
-      if (err) return handleError(req, res, err, '/rounds');
+      if (err) return handleError(req, res, err, redirect);
       
       if (req.body.round.next_round) {
         Round.
-          findOne({number: req.round + 1}).
+          findOne({number: req.round.number + 1}).
           run(function(err, round){
-            if (err) return handleError(req, res, err, '/rounds');
-            
+            if (err) return handleError(req, res, err, redirect);
+            if (!round) return handleError(req, res, "no round found.", redirect);
+
             round.is_current = true;
             round.save(function(err) {
-              if (err) return handleError(req, res, err, '/rounds');
+              if (err) return handleError(req, res, err, redirect);
 
               req.flash('notice', 'Round progressed.');
-              res.redirect('/rounds');
+              res.redirect(redirect);
             })
           });
       }
       else
       {
         req.flash('notice', 'Round toggled.');
-        res.redirect('/rounds');
+        res.redirect(redirect);
       }
     });
+  });
+
+  //Process a round
+  app.put('/round/:roundNumber/process', function(req, res){
+    var round = req.round;
+    if (round.processed) return handleError(req, res, "cannot process again.", redirect);
+
+    //todo!!
+    round.standard_deviation = 0.25;
+
+    round.save(function(err){
+      if (err) return handleError(req, res, err, redirect);
+
+      req.flash('notice', 'Round ' + round.number.toString() + ' processed.');
+      res.redirect(redirect);        
+    });
+
   });
 
   //Allocate funds to round
@@ -120,7 +139,7 @@ module.exports = function(app){
       var self = this;
       new Transaction({amount:amount, round: number, user: user.id, label: 'round ' + number.toString() + ' allocation.'}).
         save(function(err, doc) {
-          if (err) return handleError(req, res, err, '/rounds');
+          if (err) return handleError(req, res, err, redirect);
           self.resume();
       });  
     })
@@ -128,7 +147,7 @@ module.exports = function(app){
     var errorMode = false; 
     stream.on('error', function (err) {
       req.flash('error', 'Error allocating funds.');
-      res.redirect('/rounds');
+      res.redirect(redirect);
       this.destroy();
     });
 
@@ -138,11 +157,11 @@ module.exports = function(app){
 
       round.save(function(err){
         if (err) {
-          handleError(req, res, err, '/rounds'); //TODO: should really undo all user updates above via a transaction
+          handleError(req, res, err, redirect); //TODO: should really undo all user updates above via a transaction
         } 
         else {
           req.flash('notice', 'Allocated funds to all users for round ' + req.round.number.toString() + '.');
-          res.redirect('/rounds');
+          res.redirect(redirect);
         }
       });
     });
